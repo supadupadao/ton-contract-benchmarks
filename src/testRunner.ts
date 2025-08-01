b1374d1eb1b2178be71a35e0ebb1ead207044446
Suprun <hello@syoma.website>
2025-08-01 20:53:11 +0400

    chore: add reporting

--- src/testRunner.ts
+++ src/testRunner.ts
@@ -14,8 +14,9 @@ import { TestCase, TestInitData } from './tests/base';
 import { JettonTransferTest } from './tests/jettonTransfer';
 import { JettonMaster, JettonMasterConstructor } from './contracts/base';
 import { ALICE, BOB } from './contracts/common';
-import { ContractABI } from '@ton/core';
+import { ContractABI, toNano } from '@ton/core';
 import { getJettonMasterABI, getJettonWalletABI } from './abi';
+import { appendFile, appendFileSync, writeFileSync } from 'node:fs';
 
 export class TestsRunner {
   private contracts: JettonMasterConstructor[] = [];
@@ -34,6 +35,17 @@ export class TestsRunner {
       const deployerWallet = blockchain.openContract(await jettonMaster.wallet(deployer.address));
       const alice = blockchain.openContract(await jettonMaster.wallet(ALICE));
       const bob = blockchain.openContract(await jettonMaster.wallet(BOB));
+
+      writeFileSync('README.md', '# Benchmarks of TON jetton implementations\n\n');
+      appendFileSync(
+        'README.md',
+        '| Contract Name | Jetton Master gas used | Jetton Wallet gas used |\n'
+      );
+      appendFileSync(
+        'README.md',
+        '|---------------|------------------------|------------------------|\n'
+      );
+
       for (const Test of this.tests) {
         let store = createMetricStore();
         const list: SnapshotMetric[] = [];
@@ -56,13 +68,28 @@ export class TestsRunner {
 
         await testInstance.run();
 
-        const m = makeSnapshotMetric(store, { contractDatabase });
-        console.log('METRIC', m);
-        list.push(m);
+        await deployerWallet.getWalletData();
+        await deployerWallet.sendTransfer(deployer.getSender(), alice.address, 1n);
 
-        const delta = makeGasReport(list);
-        // console.log(JSON.stringify(contractDatabase.data, null, 2));
-        console.log(list, gasReportTable(delta, defaultColor));
+        const metric = makeSnapshotMetric(store, { contractDatabase });
+
+        const jettonMasterReport = metric.items.reduce((acc, item) => {
+          if (item.codeHash == `0x${jettonMaster.init.code.hash().toString('hex')}`) {
+            acc += item.execute.compute.gasUsed || 0;
+          }
+          return acc;
+        }, 0);
+        const jettonWalletReport = metric.items.reduce((acc, item) => {
+          if (item.codeHash == `0x${deployerWallet.init.code.hash().toString('hex')}`) {
+            acc += item.execute.compute.gasUsed || 0;
+          }
+          return acc;
+        }, 0);
+
+        appendFileSync(
+          'README.md',
+          `| ${Test.name} | ${jettonMasterReport} | ${jettonWalletReport} |\n`
+        );
       }
     }
   }
